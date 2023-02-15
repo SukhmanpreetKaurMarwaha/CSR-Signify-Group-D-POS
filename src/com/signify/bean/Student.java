1c44e6267df634258aaebe7fd4d8e7567da51d8b
Sukhmanpreet <skaur1_be19@thapar.edu>
2023-02-15 21:53:37 +0530

    My new commit

---
+++ src/com/signify/bean/Student.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.signify.bean;
+
+/**
+ * @author HP
+ *
+ */
+public class Student {
+	
+	private int studentId;
+	private String branch;
+	private int batch;
+		
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public int getBatch() {
+		return batch;
+	}
+	public void setBatch(int batch) {
+		this.batch = batch;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/com/signify/service/StudentService.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.signify.service;
+
+/**
+ * @author HP
+ *
+ */
+public class StudentService {
+	
+	public void changePassword()
+	{
+		System.out.println("Password Changed");
+	}
+	
+	public void register()
+	{
+		System.out.println("Student registered");
+	}
+	public void viewGradeCard()
+	{
+		System.out.println("Grade Card Viewed");
+	}
+
+}
