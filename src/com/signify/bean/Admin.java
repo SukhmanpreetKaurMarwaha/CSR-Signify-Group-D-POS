cca09e7b5ff441d7a57099ecbcd228596a1d3885
Tejasvini s j <83571965+tejasvinisj@users.noreply.github.com>
2023-02-16 10:22:04 +0530

    Add files via upload

---
+++ src/com/signify/bean/Admin.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.signify.bean;
+
+/**
+ * @author Tejaswini.S.J
+ *
+ */
+public class Admin
+{
+    private String doj;
+
+	public String getDoj() {
+		return doj;
+	}
+
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+}
