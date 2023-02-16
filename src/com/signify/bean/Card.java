a89c29006191eec5657097a3866d787d1475d57b
Tejasvini s j <83571965+tejasvinisj@users.noreply.github.com>
2023-02-16 09:37:31 +0530

    Add files via upload

---
+++ src/com/signify/bean/Card.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.signify.bean;
+
+/**
+ * @author Tejaswini.S.J
+ *
+ */
+public class Card 
+{
+     private int cardNumber;
+     private String cardType;
+    
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	
+	public String getCardType() {
+		return cardType;
+	}
+	
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+	
+}
---
+++ src/com/signify/bean/Cheque.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.signify.bean;
+
+/**
+ * @author Tejaswini.S.J
+ *
+ */
+public class Cheque 
+{
+        private String bankName;
+        private int chequeNumber;
+        public String getBankName() {
+			return bankName;
+		}
+		public void setBankName(String bankName) {
+			this.bankName = bankName;
+		}
+		public int getChequeNumber() {
+			return chequeNumber;
+		}
+		public void setChequeNumber(int chequeNumber) {
+			this.chequeNumber = chequeNumber;
+		}
+		
+}
