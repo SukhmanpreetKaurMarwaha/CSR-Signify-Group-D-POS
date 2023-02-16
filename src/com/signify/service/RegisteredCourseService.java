aacb0605096c90aadb0dd02f7897646d549e01ec
Tejasvini s j <83571965+tejasvinisj@users.noreply.github.com>
2023-02-16 09:38:18 +0530

    Add files via upload

---
+++ src/com/signify/service/RegisteredCourseService.java
@@ -0,0 +1,14 @@
+package com.signify.service;
+
+public class RegisteredCourseService {
+
+	public void viewGrade(int studentId)
+    {
+  	  System.out.println("you can view your grade");
+    }
+	public void dropCourse(String courseCode)
+    {
+  	  System.out.println("course is dropped");
+    }
+
+}
